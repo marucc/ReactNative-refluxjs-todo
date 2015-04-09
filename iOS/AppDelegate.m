4a69fd832cde12b5b1a8977573bdea3b00823475
Chris Anderson <jchris@couchbase.com>
2015-04-09 16:42:08 -0700

    really point at localhost

--- iOS/AppDelegate.m
+++ iOS/AppDelegate.m
@@ -37,7 +37,7 @@
   // $ curl 'http://localhost:8081/index.ios.bundle?dev=false&minify=true' -o iOS/main.jsbundle
   //
   // and uncomment the next following line
-  jsCodeLocation = [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
+  // jsCodeLocation = [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
 
   [self launchCouchbaseLite];
   
